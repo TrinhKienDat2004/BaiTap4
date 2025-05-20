07b497940d6f301788a0bf28cfb9c33432290e77
TrinhKienDat2004 <Trinhkiendat2004@gmail.com>
2025-05-20 15:11:41 +0700

    Thêm toàn bộ file

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bai4_Web.Controllers
+{
+	public class ServicesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- obj/Debug/net8.0/Bai4_Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bai4_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai4_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b206627ce83fd7b48f07cc695b02cab2b2687f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai4_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai4_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
